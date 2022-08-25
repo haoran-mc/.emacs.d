82f28aaf03b13f90f5d4beda13f92c4c63905b3d
haoran <haoran.mc@outlook.com>
2022-08-25 16:05:30 +0800

    open in vscode

--- lisp/init-funcs.el
+++ lisp/init-funcs.el
@@ -215,5 +215,15 @@ the current layouts buffers."
       (find-file (concat "/sudo:root@localhost:" (ido-read-file-name "File: ")))
     (find-alternate-file (concat "/sudo:root@localhost:" buffer-file-name))))
 
+;;;###autoload
+(defun +open-with-vscode ()
+  "Open current file with vscode."
+  (interactive)
+  (let ((line (number-to-string (line-number-at-pos)))
+        (column (number-to-string (current-column))))
+    (apply 'call-process "code" nil nil nil (list
+                                             (concat buffer-file-name ":" line ":" column)
+                                             "--goto"))))
+
 (provide 'init-funcs)
 ;;; init-funcs.el ends here
