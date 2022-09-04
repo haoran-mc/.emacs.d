266b00a9afa8f6b4621491cbd6206cbedd1c1339
haoran <haoran.mc@outlook.com>
2022-09-04 17:40:04 +0800

    remove funcs, custom and the hook I don't understand, use aweshell

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -20,69 +20,23 @@
 ;; the Emacs shell & friends
 (use-package eshell
   :ensure nil
-  :hook ((eshell-mode . (lambda ()
-                          (shell-mode-common-init)
-                          ;; Eshell is not fully functional
-                          (setenv "PAGER" "cat"))))
   :config
   ;; Prevent accident typing
   (defalias 'eshell/vi 'find-file)
   (defalias 'eshell/vim 'find-file)
   (defalias 'eshell/nvim 'find-file)
 
-  (defun eshell/bat (file)
-    "cat FILE with syntax highlight."
-    (with-temp-buffer
-      (insert-file-contents file)
-      (let ((buffer-file-name file))
-        (delay-mode-hooks
-          (set-auto-mode)
-          (font-lock-ensure)))
-      (buffer-string)))
-
-  (defun eshell/f (filename &optional dir)
-    "Search for files matching FILENAME in either DIR or the
-current directory."
-    (let ((cmd (concat
-                (executable-find "find")
-                " " (or dir ".")
-                "      -not -path '*/.git*'"
-                " -and -not -path 'build'"    ;; the cmake build directory
-                " -and"
-                " -type f"
-                " -and"
-                " -iname '*" (format "%s" filename) "*'")))
-      (eshell-command-result cmd)))
-
-  (defun eshell/z ()
-    "cd to directory with completions."
-    (let ((dir (completing-read "Directory: " (delete-dups (ring-elements eshell-last-dir-ring)) nil t)))
-      (eshell/cd dir)))
-
-  (defun eshell/bd ()
-    "cd to parent directory with completions."
-    (let ((dir default-directory)
-          dirs)
-      (while (not (string-empty-p dir))
-        (push (file-name-directory dir) dirs)
-        (setq dir (substring dir 0 -1)))
-      (let ((dir (completing-read "Directory: " dirs nil t)))
-        (eshell/cd dir))))
+  ;; git clone git@github.com:manateelazycat/aweshell.git
+  (use-package aweshell
+    :load-path "~/.emacs.d/etc/aweshell"
+    :init
+    (require 'aweshell)
+    :bind(("C-c e n"   . aweshell-new)
+          ("C-c e t"   . aweshell-toggle)
+          ("C-c e d" . aweshell-dedicated-toggle)
+          ))
   :custom
-  (eshell-banner-message "")
-  ;; The following cmds will run on term.
-  (eshell-visual-commands '("top" "htop" "less" "more" "telnet"))
-  (eshell-visual-subcommands '(("git" "help" "lg" "log" "diff" "show")))
-  (eshell-visual-options '(("git" "--help" "--paginate")))
-  (eshell-destroy-buffer-when-process-dies t)
-  ;; Completion like bash
-  (eshell-cmpl-ignore-case t)
-  (eshell-cmpl-cycle-completions nil)
-  ;; (eshell-prompt-function
-  ;;       (lambda ()
-  ;;         (concat (abbreviate-file-name (eshell/pwd))
-  ;;                 " $ ")))
-  )
+  (eshell-banner-message ""))
 
 
 (use-package em-rebind
@@ -92,8 +46,8 @@ current directory."
 (use-package esh-mode
   :ensure nil
   :bind (:map eshell-mode-map
-         ([remap kill-region] . backward-kill-word)
-         ([remap delete-char] . eshell-delchar-or-maybe-eof))
+              ([remap kill-region] . backward-kill-word)
+              ([remap delete-char] . eshell-delchar-or-maybe-eof))
   :config
   ;; Delete the last "word"
   (dolist (ch '(?_ ?- ?.))
