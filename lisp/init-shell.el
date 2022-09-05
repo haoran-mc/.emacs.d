e9b2138e23ed4634c88403535a2a9c5d606c0df0
haoran <haoran.mc@outlook.com>
2022-09-05 21:34:50 +0800

    add eshell alias

--- lisp/init-shell.el
+++ lisp/init-shell.el
@@ -22,7 +22,45 @@
 ;; the Emacs shell & friends
 (use-package eshell
   :ensure nil
+  :functions eshell/alias
+  :hook (eshell-mode . (lambda ()
+                         (bind-key "C-l" 'eshell/clear eshell-mode-map)
+                         (eshell/alias "e" "find-file $1")
+                         (eshell/alias "fo" "find-file-other-window $1")
+                         (eshell/alias "d" "dired $1")
+                         (eshell/alias "l" "ls -lFh")
+                         (eshell/alias "ll" "ls -l")
+                         (eshell/alias "la" "ls -lAFh")
+                         (eshell/alias "lr" "ls -tRFh")
+                         (eshell/alias "lrt" "ls -lFcrt")
+                         (eshell/alias "lsa" "ls -lah")))
   :config
+  (with-no-warnings
+    (defun eshell/clear ()
+      "Clear the eshell buffer."
+      (interactive)
+      (let ((inhibit-read-only t))
+        (erase-buffer)
+        (eshell-send-input)))
+    (defalias 'eshell/c #'eshell/clear)
+
+    (defun eshell/emacs (&rest args)
+      "Open a file (ARGS) in Emacs.  Some habits die hard."
+      (if (null args)
+          ;; If I just ran "emacs", I probably expect to be launching
+          ;; Emacs, which is rather silly since I'm already in Emacs.
+          ;; So just pretend to do what I ask.
+          (bury-buffer)
+        ;; We have to expand the file names or else naming a directory in an
+        ;; argument causes later arguments to be looked for in that directory,
+        ;; not the starting directory
+        (mapc #'find-file (mapcar #'expand-file-name (flatten-tree (reverse args))))))
+    (defalias 'eshell/e #'eshell/emacs)
+    (defalias 'eshell/vi #'find-file)
+    (defalias 'eshell/vim #'find-file)
+    (defalias 'eshell/nvim #'find-file)
+    (defalias 'eshell/q #'eshell/exit))
+
   ;; git clone git@github.com:manateelazycat/aweshell.git ~/.emacs.d/etc/
   (use-package aweshell
     :load-path "~/.emacs.d/etc/aweshell"
