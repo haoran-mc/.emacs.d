b0da6836cd785e1bc944cc7022c2122385bb5322
haoran <haoran.mc@outlook.com>
2022-09-23 21:56:13 +0800

    template for algorithm(cpp, go)

---
+++ templates/default-go.go
@@ -0,0 +1,6 @@
+//    filename: `(file-name-nondirectory (buffer-file-name))`
+//      author: `user-full-name`
+//    datetime: `(format-time-string "%Y-%m-%d %H:%m %a")`
+//      source：
+//   algorithm：
+// description：
