a5068df0441411d19d4329f507efb2a3dc75977a
haoran <haoran.mc@outlook.com>
2022-09-23 10:35:54 +0800

    config cpp for algorithm

--- lisp/lang/init-cpp.el
+++ lisp/lang/init-cpp.el
@@ -5,6 +5,32 @@
 
 ;;; Code:
 
+;; C/C++ Mode
+(use-package cc-mode
+  :ensure nil
+  :bind (:map c-mode-base-map
+              ("C-c C-c" . +compile-file)
+              ("<f9>"    . +run-file)
+              ("<f10>"   . gud-gdb))
+  :hook (c-mode-common . (lambda () (c-set-style "stroustrup")))
+  :init (setq-default c-basic-offset 4)
+  :config
+  (defun +compile-file ()
+    (interactive)
+    (compile
+     (format "g++ -o %s %s -g -lm -Wall"
+             (file-name-sans-extension (buffer-name))
+             (buffer-name))))
+
+  (defun +run-file ()
+    (interactive)
+    (compile
+     (format "g++ -o ac %s -g -lm -Wall"
+             (buffer-name))))
+
+  (use-package modern-cpp-font-lock
+    :diminish
+    :init (modern-c++-font-lock-global-mode t)))
 
 (provide 'init-cpp)
 
