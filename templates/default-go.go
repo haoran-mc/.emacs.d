28acec26938a56db2bdecfcc73b05ef30470dabf
haoran <haoran.mc@outlook.com>
2022-10-11 22:47:12 +0800

    template add email

--- templates/default-go.go
+++ templates/default-go.go
@@ -1,5 +1,6 @@
 //    filename: `(file-name-nondirectory (buffer-file-name))`
 //      author: `user-full-name`
+//       email: `user-mail-address`
 //    datetime: `(format-time-string "%Y-%m-%d %H:%m %a")`
 //      source：
 //   algorithm：
