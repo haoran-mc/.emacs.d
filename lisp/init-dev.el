5efde6651dfee53896b2c2d25db62c1d78234800
haoran <haoran.mc@outlook.com>
2023-04-28 23:54:48 +0800

    remove citre

--- lisp/init-dev.el
+++ lisp/init-dev.el
@@ -145,21 +145,6 @@
   (dumb-jump-aggressive t)
   (dumb-jump-selector 'completing-read))
 
-;; A fancy ctags frontend
-(use-package citre
-  :ensure t
-  :init
-  ;; Load the prelude.
-  (require 'citre-config)
-  :bind (("C-c c j" . citre-jump)
-         ("C-c c J" . citre-jump-back)
-         ("C-c c p" . citre-peek)
-         ("C-c c a" . citre-ace-peek)
-         ("C-c c u" . citre-update-this-tags-file))
-  :custom
-  (citre-auto-enable-citre-mode-modes '(prog-mode))
-  (citre-prompt-language-for-ctags-command t))
-
 ;; Browse devdocs.io
 (use-package devdocs
   :ensure t
