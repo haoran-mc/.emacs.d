e427023b69f526404c0b3b2bab34068a51c536d9
haoran <haoran.mc@outlook.com>
2023-12-05 10:53:18 +0800

    gpl3 license

--- lisp/init-dired.el
+++ lisp/init-dired.el
@@ -1,4 +1,23 @@
 ;;; init-dired.el --- dired tweaks -*- lexical-binding: t -*-
+
+;; Copyright (C) 2022  Haoran Liu
+
+;; Author: HaoRan Liu <haoran.mc@outlook.com>
+;; Keywords:
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;;
 ;; dired-narrow is superseded by `consult-focus-lines'.
