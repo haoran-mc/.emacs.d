82c87a64111b16e85fe3d870565afcef5f120a0d
haoran <haoran.mc@outlook.com>
2023-12-23 17:30:50 +0800

    text in emacs

--- lisp/init-text.el
+++ lisp/init-text.el
@@ -25,67 +25,69 @@
 
 ;;; Code:
 
+;; valign ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Pixel alignment for org/markdown tables
-(use-package valign
-  :ensure t
-  :hook ((markdown-mode org-mode) . valign-mode)
-  :custom
-  (valign-fancy-bar t))
+(require 'valign)
+(dolist (mode-hook '(markdown-mode org-mode))
+  (add-hook mode-hook 'valign-mode))
+(setq valign-fancy-bar t)
 
+
+;; valign ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Type text
-(use-package text-mode
-  :ensure nil
-  :custom
-  ;; better word wrapping for CJK characters
-  (word-wrap-by-category t)
-  ;; paragraphs
-  (sentence-end-double-space nil))
+(require 'text-mode)
+;; better word wrapping for CJK characters
+(setq word-wrap-by-category t
+      ;; paragraphs
+      sentence-end-double-space nil)
 
+
+;; writeroom-mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; another nice writing environment
-(use-package writeroom-mode
-  :ensure t
-  :commands (writeroom-mode))
+;; (require 'writeroom-mode)
 
+
+;; markdown ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; The markdown mode is awesome! unbeatable
-(use-package markdown-mode
-  :ensure t
-  :mode (("README\\.md\\'" . gfm-mode))
-  :init
-  (setq markdown-enable-wiki-links t
-        markdown-italic-underscore t
-        markdown-asymmetric-header t
-        markdown-make-gfm-checkboxes-buttons t
-        markdown-gfm-uppercase-checkbox t
-        markdown-fontify-code-blocks-natively t)
+(require 'markdown-mode)
+(with-eval-after-load 'markdown-mode
+  (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode)))
 
-  :config
-  (with-no-warnings
-    ;; Use `which-key' instead
-    (advice-add #'markdown--command-map-prompt :override #'ignore)
-    (advice-add #'markdown--style-map-prompt   :override #'ignore))
 
-  (defun +func-markdown-insert-ruby-tag (text ruby)
-    "Insert ruby tag with `TEXT' and `RUBY' quickly."
-    (interactive "sText: \nsRuby: \n")
-    (insert (format "<ruby>%s<rp>(</rp><rt>%s</rt><rp>)</rp></ruby>" text ruby)))
+(setq markdown-enable-wiki-links t
+      markdown-italic-underscore t
+      markdown-asymmetric-header t
+      markdown-make-gfm-checkboxes-buttons t
+      markdown-gfm-uppercase-checkbox t
+      markdown-fontify-code-blocks-natively t)
 
-  (defun +func-markdown-insert-details (title)
-    "Insert details tag (collapsible) quickly."
-    (interactive "sTitle: ")
-    (insert (format "<details><summary>%s</summary>\n\n</details>" title)))
+(with-no-warnings
+  ;; Use `which-key' instead
+  (advice-add #'markdown--command-map-prompt :override #'ignore)
+  (advice-add #'markdown--style-map-prompt   :override #'ignore))
 
-  :config
-  ;; Table of contents
-  (use-package markdown-toc
-    :ensure t)
+(defun +func-markdown-insert-ruby-tag (text ruby)
+  "Insert ruby tag with `TEXT' and `RUBY' quickly."
+  (interactive "sText: \nsRuby: \n")
+  (insert (format "<ruby>%s<rp>(</rp><rt>%s</rt><rp>)</rp></ruby>" text ruby)))
 
-  ;; sudo pip install grip or pip install grip
-  ;; use grip-mode in markdown-mode and org-mode
-  (use-package grip-mode
-    :ensure t
-    :defines org-mode-map
-    :config
-    (setq grip-update-after-change nil)))
+(defun +func-markdown-insert-details (title)
+  "Insert details tag (collapsible) quickly."
+  (interactive "sTitle: ")
+  (insert (format "<details><summary>%s</summary>\n\n</details>" title)))
+
+
+;; markdown-toc ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Table of contents
+;; (require 'markdown-toc)
+
+
+;; grip-mode ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; sudo pip install grip or pip install grip
+;; use grip-mode in markdown-mode and org-mode
+(require 'grip-mode)
+(setq grip-update-after-change nil)
+
 
 (provide 'init-text)
 ;;; init-text.el ends here
