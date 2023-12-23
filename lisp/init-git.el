c7b10b5233e255e0f2ccb59c5c75a5a4ebaeba3f
haoran <haoran.mc@outlook.com>
2023-12-23 17:06:09 +0800

    git in emacs

--- lisp/init-git.el
+++ lisp/init-git.el
@@ -28,66 +28,58 @@
 ;;
 ;; Explicit binding makes it load lazily although it's the default.
 ;; See `magit-define-global-key-bindings' for more information.
-(use-package magit
-  :ensure t
-  :bind (([remap magit-mode-bury-buffer] . kill-this-buffer)
-         ("C-x g"   . magit-status)
-         :map magit-status-mode-map
-         ("Q"       . quit-window))
-  :custom
-  (magit-diff-refine-hunk t)
-  (magit-diff-paint-whitespace nil)
-  (magit-ediff-dwim-show-on-hunks t))
+;; magit ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq magit-diff-refine-hunk t
+      magit-diff-paint-whitespace nil
+      magit-ediff-dwim-show-on-hunks t)
 
+
+;; vc ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; NOTE: `diff-hl' depends on `vc'
-(use-package vc
-  :ensure nil
-  :custom
-  (vc-follow-symlinks t)
-  (vc-allow-async-revert t)
-  (vc-handled-backends '(Git)))
+(require 'vc)
+(setq vc-follow-symlinks t
+      vc-allow-async-revert t
+      vc-handled-backends '(Git))
 
+
+;; diff-hl ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Highlight uncommitted changes using VC
-(use-package diff-hl
-  :ensure t
-  :hook (;; (after-init         . global-diff-hl-mode)
-         (go-mode            . diff-hl-mode)
-         (emacs-lisp-mode    . diff-hl-mode)
-         (dired-mode         . diff-hl-dired-mode-unless-remote)
-         (magit-pre-refresh  . diff-hl-magit-pre-refresh)
-         (magit-post-refresh . diff-hl-magit-post-refresh))
-  :when (display-graphic-p))
-  ;; When Emacs runs in terminal, show the indicators in margin instead.
-  ;; (unless (display-graphic-p)
-  ;;   (diff-hl-margin-mode))
+(require 'diff-hl)
+(dolist (mode-hook '(go-mode-hook
+                     emacs-lisp-mode-hook))
+  (add-hook mode-hook 'diff-hl-mode))
+(add-hook 'dired-mode-hook 'diff-hl-dired-mode-unless-remote)
+(add-hook 'magit-pre-refresh-hook 'diff-hl-magit-pre-refresh)
+(add-hook 'magit-post-refresh-hook 'diff-hl-magit-post-refresh)
 
+
+;; ediff ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; BUILT-IN: Visual diff interface
-(use-package ediff
-  :ensure nil
-  ;; Restore window config after quitting ediff
-  :hook ((ediff-before-setup . ediff-save-window-conf)
-         (ediff-quit         . ediff-restore-window-conf))
-  :config
-  (defvar local-ediff-saved-window-conf nil)
+(require 'ediff)
+(add-hook 'ediff-before-setup-hook 'ediff-save-window-conf)
+(add-hook 'ediff-quit 'ediff-restore-window-conf)
+(defvar local-ediff-saved-window-conf nil)
 
-  (defun ediff-save-window-conf ()
-    (setq local-ediff-saved-window-conf (current-window-configuration)))
+(defun ediff-save-window-conf ()
+  (setq local-ediff-saved-window-conf (current-window-configuration)))
 
-  (defun ediff-restore-window-conf ()
-    (when (window-configuration-p local-ediff-saved-window-conf)
-      (set-window-configuration local-ediff-saved-window-conf)))
-  :custom
-  (ediff-highlight-all-diffs t)
-  (ediff-window-setup-function 'ediff-setup-windows-plain)
-  (ediff-split-window-function 'split-window-horizontally)
-  (ediff-merge-split-window-function 'split-window-horizontally))
+(defun ediff-restore-window-conf ()
+  (when (window-configuration-p local-ediff-saved-window-conf)
+    (set-window-configuration local-ediff-saved-window-conf)))
 
+(setq ediff-highlight-all-diffs t
+      ediff-window-setup-function 'ediff-setup-windows-plain
+      ediff-split-window-function 'split-window-horizontally
+      ediff-merge-split-window-function 'split-window-horizontally)
+
+
+;; gitignore ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Setup gitignore mode
-(use-package conf-mode
-  :ensure nil
-  :mode (("\\.gitignore\\'"     . conf-unix-mode)
-         ("\\.gitconfig\\'"     . conf-unix-mode)
-         ("\\.gitattributes\\'" . conf-unix-mode)))
+(require 'conf-mode)
+(add-to-list 'auto-mode-alist '("\\.gitignore\\'"     . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.gitconfig\\'"     . conf-unix-mode))
+(add-to-list 'auto-mode-alist '("\\.gitattributes\\'" . conf-unix-mode))
+
 
 (provide 'init-git)
 ;;; init-git.el ends here
