8175de76572c03115d0f254df7287239561dde40
haoran <haoran.mc@outlook.com>
2023-12-23 17:19:11 +0800

    search in emacs

--- lisp/init-isearch.el
+++ lisp/init-isearch.el
@@ -24,38 +24,30 @@
 
 ;;; Code:
 
+(with-eval-after-load 'isearch
+  (define-key isearch-mode-map (kbd "C-c C-o") 'isearch-occur)
+  (define-key isearch-mode-map [remap isearch-delete-char] 'isearch-del-char))
 
+(define-advice isearch-occur (:after (_regexp &optional _nlines))
+  (isearch-exit))
 
-;; The builtin incremental search
-(use-package isearch
-  :ensure nil
-  :bind (:map isearch-mode-map
-              ;; consistent with ivy-occur
-              ("C-c C-o"                   . isearch-occur)
-              ([escape]                    . isearch-cancel)
-              ;; Edit the search string instead of jumping back
-              ([remap isearch-delete-char] . isearch-del-char))
-  :config
-  (define-advice isearch-occur (:after (_regexp &optional _nlines))
-    (isearch-exit))
-  :custom
-  ;; Record isearch in minibuffer history, so C-x ESC ESC can repeat it.
-  (isearch-resume-in-command-history t)
-  ;; One space can represent a sequence of whitespaces
-  (isearch-lax-whitespace t)
-  (isearch-regexp-lax-whitespace t)
-  (isearch-repeat-on-direction-change t)
-  ;; M-< and M-> move to the first/last occurrence of the current search string.
-  (isearch-allow-motion t)
-  (isearch-motion-changes-direction t)
-  ;; lazy isearch
-  (isearch-lazy-count t)
-  (isearch-lazy-highlight t)
-  (lazy-count-prefix-format nil)
-  (lazy-count-suffix-format " [%s/%s]")
-  (lazy-highlight-buffer t)
-  ;; Mimic Vim
-  (lazy-highlight-cleanup nil))
+;; Record isearch in minibuffer history, so C-x ESC ESC can repeat it.
+(setq isearch-resume-in-command-history t
+      ;; One space can represent a sequence of whitespaces
+      isearch-lax-whitespace t
+      isearch-regexp-lax-whitespace t
+      isearch-repeat-on-direction-change t
+      ;; M-< and M-> move to the first/last occurrence of the current search string.
+      isearch-allow-motion t
+      isearch-motion-changes-direction t
+      ;; lazy isearch
+      isearch-lazy-count t
+      isearch-lazy-highlight t
+      lazy-count-prefix-format nil
+      lazy-count-suffix-format " [%s/%s]"
+      lazy-highlight-buffer t
+      ;; Mimic Vim
+      lazy-highlight-cleanup nil)
 
 
 (provide 'init-isearch)
