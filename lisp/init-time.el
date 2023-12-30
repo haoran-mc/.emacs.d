3a5238f80aa4cb2cfd7b236aeb7ef5a3e8135abe
haoran <haoran.mc@outlook.com>
2023-12-31 01:14:28 +0800

    no display day and date

--- lisp/init-time.el
+++ lisp/init-time.el
@@ -26,11 +26,11 @@
 
 (setq system-time-locale "C")
 
-(setq display-time-day-and-date t)    ;; 打开日期显示
-(display-time-mode 1)                 ;; 打开时间显示
-(display-time)                        ;; 显示时间
+;; (setq display-time-day-and-date t)    ;; 打开日期显示
 (setq display-time-format "%H:%M")    ;; 设定时间显示格式
 (setq display-time-24hr-format t)     ;; 打开24小时显示模式
+(display-time-mode 1)                 ;; 打开时间显示
+(display-time)                        ;; 显示时间
 
 (provide 'init-time)
 ;;; init-time.el ends here
