8f51f0968148b097f205e185c01d12af2a71e78a
haoran <haoran.mc@outlook.com>
2024-07-09 13:53:36 +0800

    whole line or region indent

--- lisp/init-keys.el
+++ lisp/init-keys.el
@@ -55,6 +55,14 @@
 (lazy-load-global-keys '(("C-;" . avy-goto-char)) "init-avy")
 (lazy-load-global-keys '(("C-?" . vundo)) "init-vundo") ;; keep C-/ undo, use C-? vundo instead undo-redo
 
+(lazy-load-global-keys '(("C-<" . whole-line-or-region-indent-rigidly-left-to-tab-stop)
+                         ("C->" . whole-line-or-region-indent-rigidly-right-to-tab-stop))
+                       "init-whole-line-or-region")
+(with-eval-after-load 'org
+  (lazy-load-set-keys '(("C-<" . whole-line-or-region-indent-rigidly-left-to-tab-stop)
+                        ("C->" . whole-line-or-region-indent-rigidly-right-to-tab-stop))
+                      org-mode-map))
+
 (lazy-load-global-keys '(("C-a" . mwim-beginning-of-line-or-code)
                          ("C-e" . mwim-end-of-line-or-code))
                        "mwim")
