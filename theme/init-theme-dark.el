8da1684fb874d8fd3f6d5ead3ab1dec1fa82285a
haoran <haoran.mc@outlook.com>
2024-07-25 00:26:58 +0800

    continue custom dark theme faces

--- theme/init-theme-dark.el
+++ theme/init-theme-dark.el
@@ -38,7 +38,6 @@
 ;; (set-face-attribute 'variable-pitch nil
 ;;                     :family +font-family)
 
-;; org-mode
 (with-eval-after-load 'org
   (set-face-attribute 'org-level-1 nil :inherit 'outline-1 :weight 'normal :height 1.0)
   (set-face-attribute 'org-level-2 nil :inherit 'outline-2 :weight 'normal :height 1.0)
@@ -58,7 +57,6 @@
                       :weight 'normal
                       :height 1.0))
 
-;; treemacs
 (with-eval-after-load 'treemacs
   (set-face-attribute 'treemacs-directory-face nil
                       :height 1
@@ -70,6 +68,15 @@
                       :weight 'normal
                       :family +font-family)
 
+  (set-face-attribute 'treemacs-root-face nil
+                      :height 1
+                      :weight 'normal
+                      :family +font-family)
+
+  (set-face-attribute 'doom-themes-treemacs-root-face nil
+                      :height 1
+                      :weight 'normal)
+
   ;; Git 状态相关，统一继承自 treemacs-file-face
   (set-face-attribute 'treemacs-git-added-face nil :inherit 'treemacs-file-face)
   (set-face-attribute 'treemacs-git-conflict-face nil :inherit 'treemacs-file-face)
@@ -77,17 +84,8 @@
   (set-face-attribute 'treemacs-git-modified-face nil :inherit 'treemacs-file-face)
   (set-face-attribute 'treemacs-git-renamed-face nil :inherit 'treemacs-file-face)
   (set-face-attribute 'treemacs-git-unmodified-face nil :inherit 'treemacs-file-face)
-  (set-face-attribute 'treemacs-git-untracked-face nil :inherit 'treemacs-file-face)
+  (set-face-attribute 'treemacs-git-untracked-face nil :inherit 'treemacs-file-face))
 
-  (set-face-attribute 'treemacs-root-face nil
-                      :height 1
-                      :weight 'normal
-                      :family +font-family)
-
-  (set-face-attribute 'variable-pitch nil
-                      :family nil))
-
-;; meow
 (with-eval-after-load 'meow
   ;; (set-face-attribute 'meow-keypad-cursor nil :background "#7c2902")
   ;; (set-face-attribute 'meow-insert-cursor nil :background "#014f39")
@@ -112,13 +110,12 @@
                       :foreground "#505090"
                       :background "#AACCEE"
                       :box '(:line-width -1 :color "#505090")))
-;; highlight-thing
+
 (with-eval-after-load 'highlight-thing
   (set-face-attribute 'highlight-thing nil
                       :background "#606689"
                       :foreground "white"))
 
-;; symbol-overlay
 (with-eval-after-load 'symbol-overlay
   (set-face-attribute 'symbol-overlay-face-1 nil :foreground "black" :background "#A4E57E")
   (set-face-attribute 'symbol-overlay-face-2 nil :foreground "black" :background "#8CCBEA")
@@ -129,10 +126,13 @@
   (set-face-attribute 'symbol-overlay-face-7 nil :foreground "black" :background "#1E90FF")
   (set-face-attribute 'symbol-overlay-face-8 nil :foreground "black" :background "#40E0D0"))
 
-;; vertico
 (with-eval-after-load 'vertico
   (set-face-attribute 'vertico-current nil
                       :background "#424868"))
+
+(with-eval-after-load 'hl-todo
+  (set-face-attribute 'hl-todo nil
+                      :weight 'normal))
 
 (with-eval-after-load 'tab-bar
   (set-face-attribute 'tab-bar nil
