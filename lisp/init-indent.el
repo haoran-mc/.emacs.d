69ddf84402b7ac92bc7de64bf211530af852e15b
haoran <haoran.mc@outlook.com>
2024-11-04 10:10:55 +0800

    no set indent width in just one function

--- lisp/init-indent.el
+++ lisp/init-indent.el
@@ -19,32 +19,11 @@
 ;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-
-;;
+;; 在各个语言中设置？
 
 ;;; Code:
-
-
 (setq-default indent-tabs-mode nil) ;; 使用空格缩进
 (setq-default tab-width 4) ;; 制表符宽度为 4 个空格
-
-
-;; 定义一个函数统一设置
-(defun adjust-languages-indent (n)
-  (setq-local javascript-indent-level n)
-  (setq-local js-indent-level n)
-  (setq-local js2-basic-offset n)
-
-  (setq-local web-mode-attr-indent-offset n)
-  (setq-local web-mode-attr-value-indent-offset n)
-  (setq-local web-mode-code-indent-offset n)
-  (setq-local web-mode-css-indent-offset n)
-  (setq-local web-mode-markup-indent-offset n)
-  (setq-local web-mode-sql-indent-offset n)
-
-  (setq-local css-indent-offset n)
-
-  (setq-local typescript-indent-level n))
 
 
 (dolist (hook (list
@@ -54,13 +33,30 @@
                'json-mode-hook))   ;; js-indent-level 2
   (add-hook hook #'(lambda ()
                      (setq indent-tabs-mode nil)
-                     (adjust-languages-indent 2))))
+                     (setq-local javascript-indent-level 2)
+                     (setq-local js-indent-level 2)
+                     (setq-local js2-basic-offset 2)
 
+                     (setq-local web-mode-attr-indent-offset 2)
+                     (setq-local web-mode-attr-value-indent-offset 2)
+                     (setq-local web-mode-code-indent-offset 2)
+                     (setq-local web-mode-css-indent-offset 2)
+                     (setq-local web-mode-markup-indent-offset 2)
+                     (setq-local web-mode-sql-indent-offset 2)
 
-;; 配置简单，直接定义就好
-(setq yaml-indent-offset 2)
-(setq-default c-basic-offset 4)
+                     (setq-local css-indent-offset 2)
+                     (setq-local typescript-indent-level 2))))
 
+;; c, c++
+(dolist (hook (list
+               'c-mode-hook
+               'c++-mode-hook))
+  (add-hook hook #'(lambda ()
+                     (setq indent-tabs-mode nil)
+                     (setq-local c-basic-offset 4))))
+
+;; langs
+(add-hook 'yaml-mode-hook #'(lambda () (yaml-indent-offset 2)))
 
 
 (provide 'init-indent)
