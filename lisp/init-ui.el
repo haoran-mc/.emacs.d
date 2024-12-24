738cea04e8bd16747647aa48fa28c9e57f980878
haoran <haoran.mc@outlook.com>
2024-12-24 10:45:30 +0800

    light theme

--- lisp/init-ui.el
+++ lisp/init-ui.el
@@ -55,10 +55,11 @@
 (doom-themes-treemacs-config)
 (set-face-attribute 'doom-themes-treemacs-root-face nil :height 1 :weight 'normal)
 
-(load-theme 'lazycat t)
+;; (load-theme 'lazycat t)
+(load-theme 'solarized t)
 
 ;; 当前窗口透明度 (活动区 非活动区)
-(set-frame-parameter (selected-frame) 'alpha (list 90 90))
+;; (set-frame-parameter (selected-frame) 'alpha (list 90 90))
 ;; 新窗口透明度 (活动区 非活动区)
 ;; (add-to-list 'default-frame-alist (cons 'alpha (list 95 95)))
 
