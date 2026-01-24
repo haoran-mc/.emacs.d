dcb0d2afdd3a6097b82828244a022d9a61885bf6
haoran <haoran.mc@foxmail.com>
2026-01-24 21:33:42 +0800

    linux setup

---
+++ linux-setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function current_status {
+    printf "\e[33m⭑\e[0m %s\n" "$1"
+}
+
+yay -S coreutils imagemagick pandoc
+
+cp secret.el .secret.el
+
+mkdir -p ~/Documents/emacs/local-packages
+
+current_status "installation successful 🚀"
+
